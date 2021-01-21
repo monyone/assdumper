37d77c3d5ea667687e81a4f5c2c736c71ac8852d
monyone <monyone.teihen@gmail.com>
2021-01-21 14:29:02 +0900

    半角フォントで領域が重なっていたので一時的に無効に

--- subtitle/dumper.py
+++ subtitle/dumper.py
@@ -134,7 +134,7 @@ class Dumper:
     styles.append(
       self.make_style(
         Name='msz',
-        Fontname='WadaLabChuMaruGo2004ARIBHalf',
+        Fontname='WadaLabChuMaruGo2004ARIB',
         Fontsize=36 - 8, # なぜかWadaLabChuMaruGo2004ARIBがデカく描写されるので
         PrimaryColour=forground_color, #固定
         SecondaryColour=forground_color, #固定
